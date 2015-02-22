71b8e5517376806258856e0b923275f12b69ca7a
Nikolay Aleksandrov <razor@BlackWall.org>
2015-02-22 13:31:17 +0100

    tainted: update
    Update with new values for TAINT_LIVEPATCH and TAINT_SOFTLOCKUP, also
    re-write the printing code for better output.
    
    Signed-off-by: Nikolay Aleksandrov <razor@blackwall.org>

--- tainted.c
+++ tainted.c
@@ -35,46 +35,37 @@ enum {
 	TAINT_FIRMWARE_WORKAROUND,
 	TAINT_OOT_MODULE,
 	TAINT_UNSIGNED_MODULE,
+	TAINT_SOFTLOCKUP,
+	TAINT_LIVEPATCH
 };
 
 /* Extracted from linux/Documentation/sysctl/kernel.txt */
 static const char *kernel_taint_descs[] = {
-	[TAINT_PROPRIETARY_MODULE] = "A module with a non-GPL license has been loaded, this\nincludes modules with no license.\nSet by modutils >= 2.4.9 and module-init-tools",
+	[TAINT_PROPRIETARY_MODULE] = "A module with a non-GPL license has been loaded, this\n                  includes modules with no license.\n                  Set by modutils >= 2.4.9 and module-init-tools",
 	[TAINT_FORCED_MODULE] = "A module was force loaded by insmod -f",
 	[TAINT_CPU_OUT_OF_SPEC] = "Unsafe SMP processors: SMP with CPUs not designed for SMP",
 	[TAINT_FORCED_RMMOD] = "A module was forcibly unloaded from the system by rmmod -f",
 	[TAINT_MACHINE_CHECK] = "A hardware machine check error occurred on the system",
 	[TAINT_BAD_PAGE] = "A bad page was discovered on the system",
-	[TAINT_USER] = "The user has asked that the system be marked \"tainted\". This\ncould be because they are running software that directly modifies\nthe hardware, or for other reasons",
+	[TAINT_USER] = "The user has asked that the system be marked \"tainted\". This\n                  could be because they are running software that directly modifies\n                  the hardware, or for other reasons",
 	[TAINT_DIE] = "The system has died",
-	[TAINT_OVERRIDEN_ACPI_TABLE] = "The ACPI DSDT has been overridden with one supplied by the user\ninstead of using the one provided by the hardware",
+	[TAINT_OVERRIDEN_ACPI_TABLE] = "The ACPI DSDT has been overridden with one supplied by the user\n                  instead of using the one provided by the hardware",
 	[TAINT_WARN] = "A kernel warning has occurred",
 	[TAINT_CRAP] = "A module from drivers/staging was loaded",
 	[TAINT_FIRMWARE_WORKAROUND] = "The system is working around a severe firmware bug",
 	[TAINT_OOT_MODULE] = "An out-of-tree module has been loaded",
-	[TAINT_UNSIGNED_MODULE] = "An unsigned module has been loaded in a kernel supporting module\nsignature",
+	[TAINT_UNSIGNED_MODULE] = "An unsigned module has been loaded in a kernel supporting module\n                  signature",
+	[TAINT_SOFTLOCKUP] = "A soft lockup has previously occurred on the system",
+	[TAINT_LIVEPATCH] = "The kernel has been live patched",
 	NULL
 };
 
-int main(int argc, char **argv)
+static unsigned long read_flags(void)
 {
+	unsigned long flags;
 	char readbuf[16];
-	int opt, i, fd;
-	long int flags;
+	int fd;
 
-	while ((opt = getopt(argc, argv, "hi")) != -1) {
-		switch (opt) {
-		case 'i':
-			for (i = 0; kernel_taint_descs[i]; i++)
-				printf("[%d] %lu - %s\n", i, BIT(i), kernel_taint_descs[i]);
-			return 0;
-		case 'h':
-			printf(HELP_FMT, argv[0]);
-			return 0;
-		default:
-			break;
-		}
-	}
 	fd = open(PROC_TAINTED, O_RDONLY);
 	if (fd == -1) {
 		perror("open");
@@ -85,15 +76,42 @@ int main(int argc, char **argv)
 		return -errno;
 	}
 	readbuf[sizeof(readbuf) - 1] = '\0';
-	flags = strtol(readbuf, NULL, 10);
+	flags = strtoul(readbuf, NULL, 10);
 	if (errno == ERANGE || errno == EINVAL) {
-		perror("strtol");
+		perror("strtoul");
 		return -errno;
 	}
-	printf("Taint value: %ld\n", flags);
+
+	return flags;
+}
+
+int main(int argc, char **argv)
+{
+	unsigned long flags = 0;
+	int opt, i;
+
+	while ((opt = getopt(argc, argv, "hi")) != -1) {
+		switch (opt) {
+		case 'i':
+			flags = ~0;
+			break;
+		case 'h':
+			printf(HELP_FMT, argv[0]);
+			return 0;
+		default:
+			break;
+		}
+	}
+	if (!flags) {
+		flags = read_flags();
+		if (errno)
+			return -errno;
+		printf("Taint value: %lu\n", flags);
+	}
+	printf("%-5s %-11s %-64s\n", "[bit]", "[bit value]", "[description]");
 	for (i = 0; kernel_taint_descs[i] && i < sizeof(long int) * 8; i++) {
 		if (flags & BIT(i))
-			printf("[%d] %lu - %s\n",
+			printf("%-5d %-11lu %-64s\n",
 			       i, BIT(i), kernel_taint_descs[i]);
 	}
 
